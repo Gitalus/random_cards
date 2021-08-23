e73540a1e9b26e21f5db03f72ea6c2fd82a1ee65
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-23 14:19:11 -0400

    finish exercise

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+const root = document.documentElement;
 window.onload = function() {
     const randomBtn = document.getElementById('random-btn');
+    const customInput = document.querySelectorAll('input');
+
+    customInput[0].addEventListener('change', changeWidth);
+    customInput[1].addEventListener('change', changeHeight);
+
     generateCard();
 
     randomBtn.addEventListener('click', generateCard);
@@ -26,4 +32,22 @@ function randomElements() {
     const randomPinta = arrayPinta[Math.floor(Math.random() * arrayPinta.length)]
     const randomValue = arrayValue[Math.floor(Math.random() * arrayValue.length)];
     return [randomPinta, randomValue];
+}
+
+function changeWidth(event) {
+    if (event.target.value < 300) {
+        event.target.value = 300;
+        root.style.setProperty('--width-variable', '300px');
+    } else {
+        root.style.setProperty('--width-variable', event.target.value + 'px');
+    }
+}
+
+function changeHeight(event) {
+    if (event.target.value < 450) {
+        event.target.value = 450;
+        root.style.setProperty('--height-variable', '450px');
+    } else {
+        root.style.setProperty('--height-variable', event.target.value + 'px');
+    }
 }
