64043e6a232990da38fa8dcf1de724c302734458
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-23 12:09:41 -0400

    finish first purpuse of exercise

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ window.onload = function() {
     const cardElement = document.querySelector('.card');
     const [pinta, numero] = randomElements();
     const numCard = document.createTextNode(numero);
+    console.log(pinta);
 
     cardElement.appendChild(numCard);
+    cardElement.classList.add(pinta);
 
 }
 
 function randomElements() {
-    const arrayPinta = ['♦', '♥', '♠', '♣'];
+    const arrayPinta = ['diamonds', 'hearts', 'spades', 'clubs'];
     const randomPinta = arrayPinta[Math.floor(Math.random() * arrayPinta.length)]
     const randomNum = Math.floor(Math.random() * 12);
     const parsedNum = String(randomNum);
-    return [randomPinta, randomNum];
+    return [randomPinta, parsedNum];
 }
